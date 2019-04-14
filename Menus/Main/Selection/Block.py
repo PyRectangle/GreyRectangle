1e2d114f0561b14d2cb4f8770cea9ebf22634585
PyRectangle <PyRectangle@web.de>
2019-04-14 10:59:16 +0200

    Add files via upload

---
+++ Menus/Main/Selection/Block.py
@@ -0,0 +1,19 @@
+from Frame.gui.Button import Button
+from Constants import *
+from Menu import Menu
+
+
+class BlockMenu(Menu):
+    def __init__(self, menuHandler):
+        super().__init__()
+        self.main = menuHandler.main
+        self.addGui(Button, (menuHandler.main.editor.deselect, (), 1100, 975, 300, 60, "Deselect", (100, 100, 100), (0, 0, 0), (100, 100, 255), (0, 0, 0), FONT, True,
+                             30, 30, 0.1, True, [0, -1], None, SOUNDS, menuHandler.window))
+        self.addGui(Button, (menuHandler.main.editor.fill, (), 1100, 905, 300, 60, "Fill", (100, 100, 100), (0, 0, 0), (100, 100, 255), (0, 0, 0), FONT, True,
+                             30, 30, 0.1, True, [0, -1], None, SOUNDS, menuHandler.window))
+        self.addGui(Button, (menuHandler.main.editor.clone, (), 1100, 835, 300, 60, "Clone", (100, 100, 100), (0, 0, 0), (100, 100, 255), (0, 0, 0), FONT, True,
+                             30, 30, 0.1, True, [0, -1], None, SOUNDS, menuHandler.window))
+
+    def remove(self):
+        super().remove()
+        self.main.editor.deselect()
